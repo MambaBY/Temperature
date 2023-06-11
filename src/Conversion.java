0cd52efc0b07e42ce87bfdcb27f818ef744ec726
Siarhei.Paulovich <mambych@gmail.com>
2023-06-11 18:36:08 +0200

    Added method convert

--- src/Conversion.java
+++ src/Conversion.java
@@ -9,17 +9,20 @@ import java.util.Scanner;
 public class Conversion {
     public static void main(String[] args) {
         System.out.print("Enter the temperature in degrees Celsius: ");
-        Scanner sc = new Scanner(System.in);
+        convert();
 
+    }
+
+    public static void convert(){
+        Scanner sc = new Scanner(System.in);
         try {
             double temperatureCelsius = sc.nextDouble();
             System.out.println("Temperatere F is equal: " + new CelsiusToFahrenheit().converter(temperatureCelsius));
             System.out.println("Temperatere K is equal: " + new CelsiusToKelvin().converter(temperatureCelsius));
         }catch (Exception e){
             System.out.println("Invalid temperature value. Enter a number.");
-
+            convert();
         }
         sc.close();
-
     }
 }
